d8feb7187071666ca6fd833fc5ee2591cd9fbc27
sonahart <80258786+sonahart@users.noreply.github.com>
2022-07-08 17:32:19 +0100

    Write Query working

--- write_salesdata_to_sql_script.py
+++ write_salesdata_to_sql_script.py
@@ -14,26 +14,20 @@ connection = myf.create_server_connection('localhost', 'root', passkey)
 url = f'mysql://root:{passkey}@localhost/buyplus_sales'
 engine = sqa.create_engine(url, echo=True)
 
-# Create Database
-create_database_query = "CREATE DATABASE if not exists buyplus_sales"
-myf.create_database(connection, create_database_query)
-
-# Connect to Database
-connection = myf.create_db_connection(
-    "localhost", 'root', passkey, 'buyplus_sales')
-
-
-# Import OS to run through excel files
+# Use OS to run through excel files
 salesdata_path = 'C:\\Users\\kevez\\OneDrive\\Documents\\COMPANIES\\BUYPLUS - PONITEC\\DAILLY SALES'
 
-written_files = open('written_file_names.txt', 'a+')
+
+written_files = open('written_file_names.txt', 'r+')
 read_file = written_files.read()
+
+count = 0
 
 for root, dir, files in os.walk(salesdata_path):
     for file in files:
-        if file in read_file:
-            pass
-        else:
+
+        if file not in read_file:
+
             sales_df = pd.read_excel(os.path.join(root, file))
 
             # Data cleaning
@@ -42,12 +36,26 @@ for root, dir, files in os.walk(salesdata_path):
             sales_df['TIME'] = sales_df['TIME'].str.replace('P', 'PM')
             sales_df['TIME'] = sales_df['TIME'].str.replace('A', 'AM')
 
+            sales_df.drop('PROCESSBY', inplace=True, axis=1)
+            sales_df.drop('ORDERBY', inplace=True, axis=1)
+            sales_df.drop('PARTICULAR', inplace=True, axis=1)
+            sales_df.drop('SUPPLIER', inplace=True, axis=1)
+            sales_df.drop('CUSTOMERCODE', inplace=True, axis=1)
+            sales_df.drop('SALESTYPE', inplace=True, axis=1)
+            sales_df.drop('SALESPOINT', inplace=True, axis=1)
+            sales_df.drop('VAT', inplace=True, axis=1)
+
             # Write to table
             sales_df.to_sql('sales1', engine, index=False, if_exists='append')
             written_files.write(f'{file} \n')
 
             print(f'Successfully written {file} to sales1 table')
+            count += 1
 
 written_files.close()
 
+if count != 0:
+    print('All new files have been written to SQL table')
+else:
+    print('No new files to write to SQL Table')
 # %%
